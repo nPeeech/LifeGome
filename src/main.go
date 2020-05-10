5b471f6c6bd6be749b661c73590beaa5c84f9369
nPeeech <33167904+nPeeech@users.noreply.github.com>
2020-05-10 23:57:08 +0900

    test render

--- src/main.go
+++ src/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	fmt.Printf("\033[2J")
+	for i := 0; i < 100000; i++ {
+		fmt.Printf("\033[1;1H")
+		fmt.Println(i)
+		fmt.Println(i)
+		fmt.Println(i)
+	}
 }
